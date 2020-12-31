e3fab191d1951e0db38ebb5985e15ab92e90b888
DineshBabu <bdineshit@gmail.com>
2020-12-31 11:47:27 +0530

    Login and Signup removed!

--- PlantingProfits-Main/ClientApp/src/app/app.module.ts
+++ PlantingProfits-Main/ClientApp/src/app/app.module.ts
@@ -4,9 +4,7 @@ import { NgModule } from '@angular/core';
 import Amplify, { Auth } from 'aws-amplify';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
 import { FormsModule } from '@angular/forms'; 
 
 Amplify.configure({
@@ -25,8 +23,6 @@ Amplify.configure({
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    SignUpComponent,
     HomeComponent
   ],
   imports: [
